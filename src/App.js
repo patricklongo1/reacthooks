0b19608b0c243e7949213b07b6b0c354a6817f2f
Longo <plongo@indracompany.com>
2019-11-14 12:36:04 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+    const [techs, setTechs] = useState([]);
+    const [newTech, setNewTech] = useState('');
+
+    const handlerAddTech = useCallback(() => {
+        setTechs([...techs, newTech]);
+        setNewTech('');
+    }, [newTech, techs]);
+
+    useEffect(() => {
+        const storagedTechs = localStorage.getItem('techs');
+
+        if (storagedTechs) {
+            setTechs(JSON.parse(storagedTechs));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techs', JSON.stringify(techs));
+    }, [techs]);
+
+    const techsSize = useMemo(() => techs.length, [techs]);
+
     return (
-        <div className="App">
-            <h1>Opa</h1>
-        </div>
+        <>
+            <ul>
+                {techs.map(t => (
+                    <li key={t}>{t}</li>
+                ))}
+            </ul>
+            <strong>Você tem {techsSize} tecnologias.</strong> <br />
+            <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+            <button type="submit" onClick={handlerAddTech}>
+                Add tech
+            </button>
+        </>
     );
 }
 
